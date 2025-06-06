8c062217436033055b02f2accd5ebdef88722ab6
Jala3400 <jala.3400@gmail.com>
2025-06-06 23:34:41 +0200

    Listen to rust errors on +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+    import { listen } from "@tauri-apps/api/event";
+    import { onDestroy, onMount } from "svelte";
     import "../app.css";
+
+    let unlisten: (() => void) | undefined;
+
+    onMount(async () => {
+        unlisten = await listen("error", async (event) => {
+            // Handle error events
+            alert(event.payload);
+            console.error("Error event received:", event.payload);
+        });
+    });
+
+    onDestroy(() => {
+        if (unlisten) {
+            unlisten();
+        }
+    });
 </script>
 
 <slot />
