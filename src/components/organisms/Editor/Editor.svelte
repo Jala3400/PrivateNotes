18b6c8a46e19ba19eaf8eb05d581daca55b8bbaa
Jala3400 <jala.3400@gmail.com>
2025-06-09 13:41:09 +0200

    First try parsing with lezer instead of regex

--- src/components/organisms/Editor/Editor.svelte
+++ src/components/organisms/Editor/Editor.svelte
@@ -4,7 +4,8 @@
     import { EditorState } from "@codemirror/state";
     import { markdown } from "@codemirror/lang-markdown";
     import { basicSetup } from "codemirror";
-    import { createMarkdownPreviewPlugin } from "./MarkdownPreviewPlugin";
+    import { syntaxHighlighting, HighlightStyle } from "@codemirror/language";
+    import { tags } from "@lezer/highlight";
     import "./md_style.css";
 
     let editorContainer: HTMLDivElement;
@@ -12,14 +13,31 @@
     let {
         content = $bindable(`# Hello World\n\nThis is a live preview demo.`),
     } = $props();
-
     onMount(() => {
+        // Create custom markdown highlighting style
+        const markdownHighlighting = HighlightStyle.define([
+            { tag: tags.heading1, class: "md-header md-h1" },
+            { tag: tags.heading2, class: "md-header md-h2" },
+            { tag: tags.heading3, class: "md-header md-h3" },
+            { tag: tags.heading4, class: "md-header md-h4" },
+            { tag: tags.heading5, class: "md-header md-h5" },
+            { tag: tags.heading6, class: "md-header md-h6" },
+            { tag: tags.strong, class: "md-bold" },
+            { tag: tags.emphasis, class: "md-italic" },
+            { tag: tags.monospace, class: "md-code-inline" },
+            { tag: tags.link, class: "md-link" },
+            { tag: tags.url, class: "md-link-url" },
+            { tag: tags.quote, class: "md-quote" },
+            { tag: tags.list, class: "md-list-item" },
+            { tag: tags.processingInstruction, class: "md-syntax" },
+        ]);
+
         const state = EditorState.create({
             doc: content,
             extensions: [
                 basicSetup,
                 markdown(),
-                createMarkdownPreviewPlugin(),
+                syntaxHighlighting(markdownHighlighting),
                 EditorView.lineWrapping,
                 EditorView.updateListener.of((update) => {
                     if (update.docChanged) {
