46f081dbb19c4f2607bacf1e821c07177f8e0f9d
Jala3400 <jala.3400@gmail.com>
2025-06-09 14:04:59 +0200

    Also use the previous markdownHighlighting for the styles that were lost

--- src/components/organisms/Editor/Editor.svelte
+++ src/components/organisms/Editor/Editor.svelte
@@ -16,20 +16,15 @@
     onMount(() => {
         // Create custom markdown highlighting style
         const markdownHighlighting = HighlightStyle.define([
-            { tag: tags.heading1, class: "md-header md-h1" },
-            { tag: tags.heading2, class: "md-header md-h2" },
-            { tag: tags.heading3, class: "md-header md-h3" },
-            { tag: tags.heading4, class: "md-header md-h4" },
-            { tag: tags.heading5, class: "md-header md-h5" },
-            { tag: tags.heading6, class: "md-header md-h6" },
-            { tag: tags.strong, class: "md-bold" },
-            { tag: tags.emphasis, class: "md-italic" },
+            { tag: tags.heading1, class: "md-h1" },
+            { tag: tags.heading2, class: "md-h2" },
+            { tag: tags.heading3, class: "md-h3" },
+            { tag: tags.heading4, class: "md-h4" },
+            { tag: tags.heading5, class: "md-h5" },
+            { tag: tags.heading6, class: "md-h6" },
             { tag: tags.monospace, class: "md-code-inline" },
-            { tag: tags.link, class: "md-link" },
-            { tag: tags.url, class: "md-link-url" },
             { tag: tags.quote, class: "md-quote" },
             { tag: tags.list, class: "md-list-item" },
-            { tag: tags.processingInstruction, class: "md-syntax" },
         ]);
 
         const state = EditorState.create({
@@ -38,6 +33,7 @@
                 basicSetup,
                 markdown(),
                 syntaxHighlighting(classHighlighter),
+                syntaxHighlighting(markdownHighlighting),
                 EditorView.lineWrapping,
                 EditorView.updateListener.of((update) => {
                     if (update.docChanged) {
