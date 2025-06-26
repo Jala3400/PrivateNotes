7961b0da03a6119f89bb3e1fee805ce0f57daf37
Jala3400 <jala.3400@gmail.com>
2025-06-26 22:47:15 +0200

    Basic toolsbar

---
+++ src/lib/components/organisms/Toolsbar/Toolsbar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import ToolButton from "$lib/components/atoms/ToolButton.svelte";
+
+    let tools = [
+        {
+            name: "Reset the app",
+            icon: "R",
+            action: () => {
+                console.log("Reset the app");
+            },
+        },
+    ];
+</script>
+
+<div class="toolsbar">
+    {#each tools as tool}
+        <ToolButton name={tool.name} icon={tool.icon} onClick={tool.action} />
+    {/each}
+</div>
+
+<style>
+    .toolsbar {
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+        gap: 0.5em;
+
+        padding: 0.5em;
+
+        background-color: var(--background-dark-light);
+        border-right: 1px solid var(--background-dark-lighter);
+    }
+</style>
--- src/routes/Notes/+page.svelte
+++ src/routes/Notes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
     import NoteEditor from "$lib/components/templates/NoteEditor/NoteEditor.svelte";
     import Sidebar from "$lib/components/organisms/Sidebar/Sidebar.svelte";
+    import Toolsbar from "$lib/components/organisms/Toolsbar/Toolsbar.svelte";
 </script>
 
 <div class="app-layout">
+    <Toolsbar />
     <Sidebar />
     <div class="main-content">
         <NoteEditor />
@@ -13,7 +15,7 @@
 <style>
     .app-layout {
         display: grid;
-        grid-template-columns: clamp(10em, 30%, 20em) auto;
+        grid-template-columns: 3em clamp(10em, 30%, 20em) auto;
 
         height: 100%;
         width: 100%;
